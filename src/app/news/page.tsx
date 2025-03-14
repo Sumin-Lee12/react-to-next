9865c2ce13b9943a865e3efda9ffc62de989ed2d
Sumin-Lee12 <leesumin041@gmail.com>
2025-03-14 11:44:39 +0900

    first commit

---
+++ src/app/news/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewsPage = () => {
+  return (
+    <div>News(ISR)</div>
+  )
+}
+
+export default NewsPage
---
+++ src/app/todolist/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoListPage = () => {
+  return (
+    <div>TodoList(CSR)</div>
+  )
+}
+
+export default TodoListPage
